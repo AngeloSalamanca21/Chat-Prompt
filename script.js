67cb349412b930b3e7a86d9e57d4db16c62cdc3a
Angelo Salamanca <angelosalamanca21@gmail.com>
2024-12-10 18:40:35 -0500

    fix-Title and async events

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ import { CreateWebWorkerMLCEngine } from "https://cdn.jsdelivr.net/npm/@mlc-ai/w
 
 const $ = (el) => document.querySelector(el);
 
-// pongo delante de la variable un símbolo de $
-// para indicar que es un elemento del DOM
 const $form = $("form");
 const $input = $("input");
 const $template = $("#message-template");
@@ -18,71 +16,71 @@ let end = false;
 
 const SELECTED_MODEL = "gemma-2b-it-q4f32_1-MLC";
 
-const engine = await CreateWebWorkerMLCEngine(
-  new Worker("./worker.js", { type: "module" }),
-  SELECTED_MODEL,
-  {
-    initProgressCallback: (info) => {
-      $info.textContent = info.text;
-      if (info.progress === 1 && !end) {
-        end = true;
-        $loading?.parentNode?.removeChild($loading);
-        $button.removeAttribute("disabled");
-        addMessage(
-          "¡Hola! Soy un ChatGPT que se ejecuta completamente en tu navegador. ¿En qué puedo ayudarte hoy?",
-          "bot"
-        );
-        $input.focus();
-      }
-    },
-  }
-);
+(async () => {
+  const engine = await CreateWebWorkerMLCEngine(
+    new Worker("./worker.js", { type: "module" }),
+    SELECTED_MODEL,
+    {
+      initProgressCallback: (info) => {
+        $info.textContent = info.text;
+        if (info.progress === 1 && !end) {
+          end = true;
+          $loading?.parentNode?.removeChild($loading);
+          $button.removeAttribute("disabled");
+          addMessage(
+            "¡Hola! Soy un Chat que te ayuda a generar prompt´s. ¿En qué puedo ayudarte hoy?",
+            "bot"
+          );
+          $input.focus();
+        }
+      },
+    }
+  );
 
-$form.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  const messageText = $input.value.trim();
+  $form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const messageText = $input.value.trim();
 
-  if (messageText !== "") {
-    // añadimos el mensaje en el DOM
-    $input.value = "";
-  }
+    if (messageText !== "") {
+      $input.value = "";
+    }
 
-  addMessage(messageText, "user");
-  $button.setAttribute("disabled", "");
+    addMessage(messageText, "user");
+    $button.setAttribute("disabled", "");
 
-  const userMessage = {
-    role: "user",
-    content: messageText,
-  };
+    const userMessage = {
+      role: "user",
+      content: messageText,
+    };
 
-  messages.push(userMessage);
+    messages.push(userMessage);
 
-  const chunks = await engine.chat.completions.create({
-    messages,
-    stream: true,
+    const chunks = await engine.chat.completions.create({
+      messages,
+      stream: true,
+    });
+
+    let reply = "";
+
+    const $botMessage = addMessage("", "bot");
+
+    for await (const chunk of chunks) {
+      const choice = chunk.choices[0];
+      const content = choice?.delta?.content ?? "";
+      reply += content;
+      $botMessage.textContent = reply;
+    }
+
+    $button.removeAttribute("disabled");
+    messages.push({
+      role: "assistant",
+      content: reply,
+    });
+    $container.scrollTop = $container.scrollHeight;
   });
-
-  let reply = "";
-
-  const $botMessage = addMessage("", "bot");
-
-  for await (const chunk of chunks) {
-    const choice = chunk.choices[0];
-    const content = choice?.delta?.content ?? "";
-    reply += content;
-    $botMessage.textContent = reply;
-  }
-
-  $button.removeAttribute("disabled");
-  messages.push({
-    role: "assistant",
-    content: reply,
-  });
-  $container.scrollTop = $container.scrollHeight;
-});
+})();
 
 function addMessage(text, sender) {
-  // clonar el template
   const clonedTemplate = $template.content.cloneNode(true);
   const $newMessage = clonedTemplate.querySelector(".message");
 
